5da86edce75aac199514b02fd211eadcc7fac77a
Sam Belliel <sbelliel@gmail.com>
2018-07-14 02:52:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,11 +29,11 @@ function dec(n) {
 }
 
 function makeInt(n) {
-  return parseInt('n', 10)
-  //return n
+ parseInt('n', 10)
+ return n
 }
 
 function preserveDecimal(n) {
   parseFloat(`n`)
-  //return n
+  return n
 }
