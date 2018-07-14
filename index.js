813f14e5e790c6af3a9283a0e6cd114ff7ef54ba
Sam Belliel <sbelliel@gmail.com>
2018-07-14 02:34:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ function subtract(a,b) {
 var  number = a - b;
   return number
 }
+
+function multiiply(a,b) {
+var  number = a * b;
+  return number
+}
+
+function divide(a,b) {
+var  number = a / b;
+  return number
+}
